0879d85754fcb2cca9deb88c4f70316ec6cc1285
Nimra-Iman <nimraamughal5@gmail.com>
2025-11-13 18:36:28 +0500

    clear some concepts to make them easier to understand

--- name2_equal_main.py
+++ name2_equal_main.py
@@ -1,6 +1,6 @@
 def harry():
     print("this is function body ")
-# harry()
+# # harry()
 print(__name__)
 if(__name__ =="__main__"):
     harry()
@@ -9,3 +9,6 @@ if(__name__ =="__main__"):
 # is ka matlab y hua k y isi file s hi print ho rha h but agar is ki output is file ka
 # name ati yani name_equal2 ati to is ka matlab y tha k is module ko import kia gia h
 #  
+
+# is file s name=main hta kr dekho or phir name_equal_main.py m import kro is module ko (yani name2_equal_main)
+# just import kro and you will see k harry vali output show ho gi, although hm n harry ko call kia hi nhi
---
+++ oop(1).py
@@ -0,0 +1,5 @@
+def fun(name,age,gender):
+    print(name,age,gender)
+
+print(fun("nimra",2,"f"))  
+print(fun("iman",22,"g"))  
